6adee7e55ae557563db5c61d3962fc3c4cd10b31
leon_2192 <leon.strokes@gmail.com>
2023-12-06 09:38:49 -0300

    fix config pwa

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   const { userExist, handleLogout } = useContext(contextAuth) || {};
 
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
       window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js')
           .then((registration) => {
