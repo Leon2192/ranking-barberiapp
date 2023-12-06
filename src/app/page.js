3cd068a5b22262edea711f0de74ec6f4cae4c5bf
leon_2192 <leon.strokes@gmail.com>
2023-12-06 09:57:59 -0300

    fix config pwa

--- src/app/page.js
+++ src/app/page.js
@@ -4,25 +4,25 @@ import Login from './login/page'
 import { AuthProvider } from '@/app/context/contextAuth'
 import { contextAuth } from "../app/context/contextAuth";
 import { useContext, useEffect } from 'react';
-import '../../public/sw'
+//import '../../public/sw'
 
 export default function Home() {
 
   const { userExist, handleLogout } = useContext(contextAuth) || {};
 
-  useEffect(() => {
-    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-          .then((registration) => {
-            console.log('Service Worker registrado con éxito:', registration);
-          })
-          .catch((error) => {
-            console.log('Error al registrar el Service Worker:', error);
-          });
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  //     window.addEventListener('load', () => {
+  //       navigator.serviceWorker.register('/sw.js')
+  //         .then((registration) => {
+  //           console.log('Service Worker registrado con éxito:', registration);
+  //         })
+  //         .catch((error) => {
+  //           console.log('Error al registrar el Service Worker:', error);
+  //         });
+  //     });
+  //   }
+  // }, []);
 
   return (
     <AuthProvider>
