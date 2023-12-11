6da1206e57fccfd2162f19a234440efbc008bd48
leon_2192 <leon.strokes@gmail.com>
2023-12-11 13:39:38 -0300

    styles inicio

--- src/app/inicio/page.jsx
+++ src/app/inicio/page.jsx
@@ -6,10 +6,14 @@ import Image from "next/image";
 import { CiHome } from "react-icons/ci";
 import { CiBellOn } from "react-icons/ci";
 import { AiOutlineMenuUnfold } from "react-icons/ai";
+import { FaBell } from "react-icons/fa6";
 import { GrConfigure } from "react-icons/gr";
+import { FaHome } from "react-icons/fa";
 import { Divider } from "@nextui-org/react";
 import { FaMapPin } from "react-icons/fa6";
 import { FaMapLocationDot } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
 import data from "../mock.json";
 import Map from '../components/Map/Map'
 import barrios from '../barrios.json'
@@ -27,7 +31,7 @@ const Page = () => {
 
     /* CONTEXT LOGOUT */
 
-    console.log("usuario existe?",userExist);
+    console.log("usuario existe?", userExist);
 
     /* CONTEXT LOGOUT */
 
@@ -62,77 +66,47 @@ const Page = () => {
                 <h1>Donde busco?</h1>
                 <form onSubmit={handleSubmit} className='flex w-full'>
                     <Select
-                        label="Barrio"
+                        label="¿Donde te queres cortar?"
                         placeholder="Selecciona una opcion"
                         value={ubicacion}
                         onChange={handleSelectChange}
-                        style={{ width: '250px', backgroundColor: '#ffba08', color: '#fff' }}
+                        style={{ width: '250px' }}
                     >
                         {barrios.map((barrio) => (
-                            <SelectItem key={barrio.nombre} value={barrio.nombre} style={{
-                                backgroundColor: '#ffba08', color: '#fff', fontWeight: 'bold'
-                            }}>
+                            <SelectItem key={barrio.nombre} value={barrio.nombre} >
                                 {barrio.nombre}
                             </SelectItem>
                         ))}
                     </Select>
-                    {/* <Input
-                        type="text"
-                        variant="bordered"
-                        placeholder="Busca locales por zona..."
-                        className="max-w-xs"
-                        value={search}
-                        onChange={handleInputChange}
-                    /> */}
-                    {/* <Button color="secondary" type="submit" className='mt-2 ml-3'>
-                        Buscar
-                    </Button> */}
+
                 </form>
-                <BtnLogout /> 
+                <BtnLogout />
                 <Toaster
                     position="bottom-right"
                     reverseOrder={false}
                 />
 
 
-                {/* {search.length > 2 && (
-                    <div className="suggestions">
-                        {suggestions.length > 0 ? (
-                            suggestions.map((item, index) => (
-                                <div key={index} className="suggestion-item flex items-center py-2">
-                                    <FaMapLocationDot size='1.5em' color="#4f4789" className="mr-2 mb-1" />
-                                    <div className="flex flex-col">
-                                        <div className="flex items-center">
-                                            <span className="mr-2">{item.nombre}</span>
-                                            <span className="text-gray-500">{item.localidad}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        ) : (
-                            <div className="no-suggestions text-gray-500">No hay coincidencias</div>
-                        )}
-                    </div>
-                )} */}
+
             </div>
 
             {ubicacion && (
                 <Map ubicacion={ubicacion} zoom={20} />
             )}
-            <div className="fixed bottom-0 w-full bg-gray-800 text-white p-2">
-                <div className="flex justify-around max-w-screen-lg mx-auto">
+            <div className="fixed bottom-0 w-full bg-gray-900 text-white p-2">
+                <div className="flex justify-around max-w-screen-lg mx-auto p-2">
                     {/* Los íconos fijos en la parte inferior */}
                     <div className="menuButton">
-                        <CiHome style={{ color: 'white' }} size={'3em'} />
+                        <FaHome style={{ color: '#ff006e' }} size={'2.5em'} />
                     </div>
                     <div className="menuButton">
-                        <AiOutlineMenuUnfold style={{ color: 'white' }} size={'3em'} />
+                        <FaRegStar style={{ color: '#ff006e' }} size={'2.5em'} />
                     </div>
                     <div className="menuButton">
-                        <CiBellOn style={{ color: 'white' }} size={'3em'} />
+                        <FaBell style={{ color: '#ff006e' }} size={'2.5em'} />
                     </div>
                     <div className="menuButton">
-                        <GrConfigure style={{ color: 'white' }} size={'3em'} />
+                        <FaRegUserCircle style={{ color: '#ff006e' }} size={'2.5em'} />
                     </div>
                 </div>
             </div>
