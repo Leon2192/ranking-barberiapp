0a5c599ee02e69768ed6b8643fbe3119d7e3ef1e
leon_2192 <leon.strokes@gmail.com>
2023-12-12 09:16:44 -0300

    feature vista ranking

--- src/app/ranking/page.jsx
+++ src/app/ranking/page.jsx
@@ -1,15 +1,34 @@
 import Image from 'next/image'
 import React from 'react'
+import { FaHome, FaRegStar, FaRegUserCircle } from 'react-icons/fa'
+import { FaBell } from "react-icons/fa6";
 
 const page = () => {
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center">
-            <div className="text-center mb-4">
-                <Image src="/logo2.png" width={200} height={200} alt={'Logo'} />
+        <>
+            <div className="min-h-screen flex flex-col justify-center items-center">
+                <div className="text-center mb-4">
+                    <Image src="/logo2.png" width={150} height={150} alt={'Logo'} />
+                </div>
             </div>
-            
-
-        </div>
+            <div className="fixed bottom-0 w-full bg-gray-900 text-white p-2">
+                <div className="flex justify-around max-w-screen-lg mx-auto p-2">
+                    {/* Los íconos fijos en la parte inferior */}
+                    <div className="menuButton">
+                        <FaHome style={{ color: '#ff006e' }} size={'2.5em'} />
+                    </div>
+                    <div className="menuButton">
+                        <FaRegStar style={{ color: '#ff006e' }} size={'2.5em'} />
+                    </div>
+                    <div className="menuButton">
+                        <FaBell style={{ color: '#ff006e' }} size={'2.5em'} />
+                    </div>
+                    <div className="menuButton">
+                        <FaRegUserCircle style={{ color: '#ff006e' }} size={'2.5em'} />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
