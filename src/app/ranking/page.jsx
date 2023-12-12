8543536f70b203da4b5caeff60c9570d60bd4cf1
leon_2192 <leon.strokes@gmail.com>
2023-12-12 10:43:11 -0300

    fix layout ranking

--- src/app/ranking/page.jsx
+++ src/app/ranking/page.jsx
@@ -11,23 +11,6 @@ const page = () => {
                     <Image src="/logo2.png" width={150} height={150} alt={'Logo'} />
                 </div>
             </div>
-            <div className="fixed bottom-0 w-full bg-gray-900 text-white p-2">
-                <div className="flex justify-around max-w-screen-lg mx-auto p-2">
-                    {/* Los íconos fijos en la parte inferior */}
-                    <div className="menuButton">
-                        <FaHome style={{ color: '#ff006e' }} size={'2.5em'} />
-                    </div>
-                    <div className="menuButton">
-                        <FaRegStar style={{ color: '#ff006e' }} size={'2.5em'} />
-                    </div>
-                    <div className="menuButton">
-                        <FaBell style={{ color: '#ff006e' }} size={'2.5em'} />
-                    </div>
-                    <div className="menuButton">
-                        <FaRegUserCircle style={{ color: '#ff006e' }} size={'2.5em'} />
-                    </div>
-                </div>
-            </div>
         </>
     )
 }
